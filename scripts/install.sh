40c8d7ed705922637081bf0bb5b0396060215b64
Morteza Jamali <mortezajamali4241@gmail.com>
2022-09-09 11:49:12 +0000

    feat(scripts): create `exercism.sh` script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if ! command -v snap &> /dev/null; then
+  echo ">> ERROR  :: 'snap' is not installed."
+  exit 1
+fi
+
+if ! command -v exercism &> /dev/null; then
+  sudo snap install exercism
+fi
+
 # Install node modules
 npm i
 
